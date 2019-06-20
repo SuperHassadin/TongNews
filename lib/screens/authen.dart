db41ec4c86d471571d6f4c3474f80be90b052c91
Tong <Prinketthip@gmail.com>
2019-06-20 11:58:06 +0700

    update 20June

--- lib/screens/authen.dart
+++ lib/screens/authen.dart
@@ -6,6 +6,67 @@ class Authen extends StatefulWidget {
 }
 
 class _AuthenState extends State<Authen> {
+  Widget mySizebox() {
+    return SizedBox(
+      width: 3.0,
+    );
+  }
+
+  Widget showButton() {
+    return Container(
+      width: 250.0,
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: signIn(),
+          ),
+          mySizebox(),
+          Expanded(
+            child: signUp(),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget signUp() {
+    return RaisedButton(color: Colors.blue[200],
+      child: Text('Sign Up'),
+      onPressed: () {},
+    );
+  }
+
+  Widget signIn() {
+    return RaisedButton(color: Colors.blue[800],
+      child: Text('sign In',style: TextStyle(color: Colors.white),),
+      onPressed: () {},
+    );
+  }
+
+  Widget emailTextFormfield() {
+    return Container(
+      alignment: Alignment.topCenter,
+      child: Container(
+        width: 250.0,
+        child: TextFormField(
+          decoration: InputDecoration(
+              labelText: 'Email :', helperText: 'your@email.com'),
+        ),
+      ),
+    );
+  }
+
+  Widget passwordTextFromfield() {
+    return Container(
+      width: 250.0,
+      child: TextFormField(
+        obscureText: true,
+        decoration: InputDecoration(
+            labelText: 'Password :', helperText: 'More 6 Charactor'),
+      ),
+    );
+  }
+
   Widget showlogo() {
     return Container(
       alignment: Alignment.topCenter,
@@ -21,12 +82,15 @@ class _AuthenState extends State<Authen> {
   }
 
   Widget showName() {
-    return Text(
-      'tong News',
-      style: TextStyle(
-        fontSize: 32.0,
-        fontWeight: FontWeight.bold,
-        color: Colors.blue[800],
+    return Container(
+      margin: EdgeInsets.only(top: 50.0),
+      child: Text(
+        'tong News',
+        style: TextStyle(
+          fontSize: 32.0,
+          fontWeight: FontWeight.bold,
+          color: Colors.blue[800],
+        ),
       ),
     );
   }
@@ -34,6 +98,7 @@ class _AuthenState extends State<Authen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomPadding: false,
       body: Container(
         padding: EdgeInsets.only(top: 80.0),
         alignment: Alignment.topCenter,
@@ -41,6 +106,9 @@ class _AuthenState extends State<Authen> {
           children: <Widget>[
             showlogo(),
             showName(),
+            emailTextFormfield(),
+            passwordTextFromfield(),
+            showButton(),
           ],
         ),
       ),
